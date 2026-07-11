70dd10a00d63a7fbc383a6f37bcb3f13b095163e
vishupochampelly <vishnupochampelly095@gmail.com>
2026-07-11 13:16:46 +0530

    creating GAR for docker images

---
+++ terraform/artifact-registry.tf
@@ -0,0 +1,18 @@
+##########################################################
+# Artifact Registry
+##########################################################
+
+resource "google_artifact_registry_repository" "docker_repo" {
+
+  location = var.region
+
+  repository_id = "gitops-images"
+
+  description = "Docker repository for GitOps lab"
+
+  format = "DOCKER"
+
+  depends_on = [
+    google_project_service.artifact_registry_api
+  ]
+}
