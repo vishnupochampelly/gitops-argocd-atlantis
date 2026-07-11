68f46de9f7f2ebb5bb22edde8e4fce398ed8e199
vishupochampelly <vishnupochampelly095@gmail.com>
2026-07-11 22:31:02 +0530

    created deployment

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -16,3 +16,10 @@ resource "google_artifact_registry_repository" "docker_repo" {
     google_project_service.artifact_registry_api
   ]
 }
+
+resource "google_project_iam_member" "gke_artifact_registry_reader" {
+  project = var.project_id
+  role    = "roles/artifactregistry.reader"
+
+  member = "serviceAccount:${data.google_project.current.number}-compute@developer.gserviceaccount.com"
+}
