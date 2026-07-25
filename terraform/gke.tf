a68967ddf307416250e9676de81401ebe4b9fae9
vishupochampelly <vishnupochampelly095@gmail.com>
2026-07-25 22:18:20 +0530

    feat: node count 2

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -19,7 +19,7 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
 
-  initial_node_count = 2
+  initial_node_count = 1
 
   ##########################################################
   # GKE IP Allocation
